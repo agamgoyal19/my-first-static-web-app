503b59e0327997165d0f55d4651e71ff41fc69d7
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-10 13:30:19 -0700

    Update 7.js

--- src/cs212/homework/7/7.js
+++ src/cs212/homework/7/7.js
@@ -1,1 +1,5 @@
+document.getElementById('actionButton').addEventListener('click', function () {
+    var outputElement = document.getElementById('output');
+    outputElement.innerHTML = 'Button clicked! Your JavaScript is working!';
+});
 
