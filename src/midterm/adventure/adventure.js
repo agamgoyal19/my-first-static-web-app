fdb7a15fef8ffa58ac4e7bf1a0b29c68bd676a1f
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-13 14:00:24 -0700

    Update adventure.js

--- src/midterm/adventure/adventure.js
+++ src/midterm/adventure/adventure.js
@@ -19,6 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const endingImage = document.getElementById('ending-image');
         const restartButton = document.getElementById('restart-button');
 
+        gameBoard.style.background = gameState.background || '';
+        
         storyText.textContent = gameState.story;
 
         // Remove existing choice buttons
@@ -94,7 +96,8 @@ const gameData = {
      8: {
         stage: 'end',
         story: 'You continue along the left door and discover a peaceful village.',
-        image: 'https://raw.githubusercontent.com/agamgoyal19/my-first-static-web-app/main/src/midterm/adventure/village.jpg'
+        image: 'https://raw.githubusercontent.com/agamgoyal19/my-first-static-web-app/main/src/midterm/adventure/village.jpg',
+        background: url('https://raw.githubusercontent.com/agamgoyal19/my-first-static-web-app/main/src/midterm/adventure/village.jpg')
     },
     9: {
         stage: 'end',
