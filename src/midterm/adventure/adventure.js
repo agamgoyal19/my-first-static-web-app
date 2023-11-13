a37bb4d59885ca84b0848e9c47f3473ca4a6fe29
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-12 18:05:52 -0700

    Update adventure.js

--- src/midterm/adventure/adventure.js
+++ src/midterm/adventure/adventure.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function startGame() {
         gameState = {
             stage: 1,
-            story: 'You find yourself at the entrance of a dark cave. What do you do?',
+            story: 'Here you find yourself at the entrance of a dark cave.Now what you choose to do?',
             choices: [
                 { text: 'Enter the cave', consequence: 2, image: 'cave.jpg' },
                 { text: 'Turn back', consequence: 3, image: 'forest.jpg' }
@@ -66,28 +66,28 @@ document.addEventListener('DOMContentLoaded', () => {
 const gameData = {
     2: {
         stage: 2,
-        story: 'As you enter the cave, you encounter a fork in the path. Which way do you go?',
+        story: 'Now as you enter the cave, you encounter two paths. Which way you choose go?',
         choices: [
-            { text: 'Go left', consequence: 4, image: 'left.jpg' },
-            { text: 'Go right', consequence: 5, image: 'right.jpg' }
+            { text: 'Left turn', consequence: 4, image: 'left.jpg' },
+            { text: 'Right turn', consequence: 5, image: 'right.jpg' }
         ]
     },
     3: {
         stage: 'end',
-        story: 'You decide to turn back and explore the forest. You discover a hidden treasure!',
+        story: 'As you chose to turn back and explore the forest. You discover a hidden treasure!',
         image: 'https://user-images.githubusercontent.com/86164231/282337528-14cd6edd-fc69-4dd0-aed7-5fd1fdfb8dde.jpg'
     },
     4: {
         stage: 4,
-        story: 'You go left and find a mysterious door. What do you do?',
+        story: 'You take a left turn and now here is a mysterious door. What you will choose to do?',
         choices: [
-            { text: 'Open the door', consequence: 6, image: 'door.jpg' },
-            { text: 'Continue straight', consequence: 7, image: 'straight.jpg' }
+            { text: 'Go towrds door and open it', consequence: 6, image: 'door.jpg' },
+            { text: 'Go straight', consequence: 7, image: 'straight.jpg' }
         ]
     },
     5: {
         stage: 5,
-        story: 'You go right and encounter a deep chasm. What\'s your next move?',
+        story: 'You take a right turn and here you encounter a deep chasm. What\'s your next move?',
         choices: [
             { text: 'Jump across', consequence: 8, image: 'jump.jpg' },
             { text: 'Find another path', consequence: 9, image: 'another.jpg' }
@@ -95,22 +95,22 @@ const gameData = {
     },
     6: {
         stage: 'end',
-        story: 'You open the door and discover a hidden treasure chest!',
+        story: 'You opened the door and now here is a hidden treasure chest!',
         image: 'https://user-images.githubusercontent.com/86164231/282337526-905f9420-f203-4074-aa86-9a4da951338b.jpg'
     },
     7: {
         stage: 'end',
-        story: 'You continue straight and find an ancient artifact!',
+        story: 'You continued straight and now here is  an ancient artifact!',
         image: 'https://user-images.githubusercontent.com/86164231/282337529-5df861b5-8f96-474e-8a56-aedaef9cc76b.jpg'
     },
     8: {
         stage: 'end',
-        story: 'You successfully jump across the chasm and find a magical portal!',
+        story: 'You successfully jumped across the chasm and here is a magical portal!',
         image: 'https://user-images.githubusercontent.com/86164231/282337531-3f1181ac-fb66-4cb6-a715-62634f0f2bcf.jpg'
     },
     9: {
         stage: 'end',
-        story: 'You wisely find another path and stumble upon a secret garden!',
+        story: 'You wisely found another path and stumbled upon a secret garden!',
         image: 'https://user-images.githubusercontent.com/86164231/282337532-2bce1524-1d4e-4323-a7d4-455613b4e753.jpg'
     }
 };
