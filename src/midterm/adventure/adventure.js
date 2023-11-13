3b1fae90206f14b3a8d261291a89ed30c92a49cb
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-13 12:50:32 -0700

    Update adventure.js

--- src/midterm/adventure/adventure.js
+++ src/midterm/adventure/adventure.js
@@ -94,7 +94,7 @@ const gameData = {
      8: {
         stage: 'end',
         story: 'You continue along the left door and discover a peaceful village.',
-        image: 'village.jpg'
+        image: 'images/village.jpg'
     },
     9: {
         stage: 'end',
