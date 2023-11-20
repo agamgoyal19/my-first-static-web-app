9d29a19ceb9cf8eaf83a0140e6cbe2b4eb605f15
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-20 09:48:48 -0700

    Update 7.js

--- src/cs212/homework/7/7.js
+++ src/cs212/homework/7/7.js
@@ -2,4 +2,12 @@ document.getElementById('actionButton').addEventListener('click', function () {
     var outputElement = document.getElementById('output');
     outputElement.innerHTML = 'Button clicked! Your JavaScript is working!';
 });
+document.getElementById('checkBox').addEventListener('click', function () {
+    var outputElement = document.getElementById('output');
+    if (this.checked) {
+        outputElement.innerHTML = 'You have clicked the checkbox!';
+    } else {
+        outputElement.innerHTML = ''; // Clear the output if the checkbox is unchecked
+    }
+});
 
