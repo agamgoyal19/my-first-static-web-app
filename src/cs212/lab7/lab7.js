8f540ab18485e012183443449fe9114ad1016797
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-25 12:15:03 -0700

    Update lab7.js

--- src/cs212/lab7/lab7.js
+++ src/cs212/lab7/lab7.js
@@ -1,22 +1,24 @@
-// madlib.js
-
-// Accessing the form
-const madLibForm = document.querySelector('form');
-
 // Function to handle form submission
 function handleSubmit(event) {
   event.preventDefault(); // Prevent default form submission behavior
 
   // Collect input values from the form
   const pluralNoun = document.getElementById('pluralNoun').value;
+  const adjective = document.getElementById('adjective').value;
+  const verb = document.getElementById('verb').value;
+  const adverb = document.getElementById('adverb').value;
+  const color = document.getElementById('color').value;
+  const animal = document.getElementById('animal').value;
+  const bodyPart = document.getElementById('bodyPart').value;
+  const occupation = document.getElementById('occupation').value;
+  const food = document.getElementById('food').value;
+  const emotion = document.getElementById('emotion').value;
   // Add other input fields as needed
 
-  // Perform operations or validations on the inputs if necessary
-  
   // Constructing the Mad Lib (this is a simple example)
-  const madLibResult = `The plural noun you entered is: ${pluralNoun}`;
+  const madLibResult = `The ${adjective} ${pluralNoun} ${verb} ${adverb} in the ${color} ${animal}'s ${bodyPart} while ${occupation} was eating ${food} feeling ${emotion}.`;
   // You can construct your Mad Lib based on the collected inputs
-  
+
   // Display the result or take further actions
   console.log(madLibResult);
   // You could display the result on the page or send it to the server via AJAX
@@ -28,4 +30,4 @@ function handleSubmit(event) {
 }
 
 // Adding a listener for form submission
-madLibForm.addEventListener('submit', handleSubmit);
+document.querySelector('form').addEventListener('submit', handleSubmit);
