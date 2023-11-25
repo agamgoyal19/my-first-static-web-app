1384cbe88e851c8e1ab14f0ac67146cf522a581e
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-25 13:35:29 -0700

    Update lab7.js

--- src/cs212/lab7/lab7.js
+++ src/cs212/lab7/lab7.js
@@ -19,7 +19,7 @@ function handleSubmit(event) {
   // Add other input fields as needed
 
   // Constructing the Mad Lib
-  const madLibResult = `In a ${adjective} land, there were ${pluralNoun} ${color} ${animal}s, with ${bodyPart} of ${occupation} enjoying eating ${food} feeling ${emotion}!`;
+  const madLibResult = `In this ${adjective} land, there were ${pluralNoun} ${color} ${animal}s that have ${bodyPart} for doing ${occupation} and enjoying eating ${food} while feeling ${emotion}!`;
   // You can construct your Mad Lib based on the collected inputs
 
   // Display the result or take further actions
