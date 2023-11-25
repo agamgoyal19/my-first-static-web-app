4ef72576b9e329511f981a092ecba8dbd32a0267
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-11-25 11:56:18 -0700

    Update server.js

--- src/cs212/lab7/server.js
+++ src/cs212/lab7/server.js
@@ -1,27 +1,32 @@
-const express = require('express')
-const logger = require('morgan')
-const path = require('path')
-const server = express()
-server.use(express.urlencoded({'extended': true}))
-server.use(logger('dev'))
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const server = express();
+
+server.use(express.urlencoded({ 'extended': true }));
+server.use(logger('dev'));
 
 // Routes
-server.get('/do_a_random', (req, res) => {
-  res.send(`Your number is: ${Math.floor(Math.random() * 100) + 1}`)
-})
+server.get('/cs212/lab7', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'lab7.html'));
+});
 
+server.post('/cs212/lab7', (req, res) => {
+  const { pluralNoun, adjective, verb, adverb } = req.body;
 
-// Setup static page serving for all the pages in "public"
-const publicServedFilesPath = path.join(__dirname, 'public')
-server.use(express.static(publicServedFilesPath))
+  // Simple Mad Lib construction (customize as needed)
+  const madLib = `The ${adjective} ${pluralNoun} ${verb} ${adverb}.`;
+  
+  // Sending the result to the EJS template
+  res.render('lab7result', { madLib });
+});
 
+// Static files serving
+server.use(express.static(path.join(__dirname, 'public')));
 
-// The server uses port 80 by default unless you start it with the extra
-// command line argument 'local' like this:
-//       node server.js local
-let port = 80
-if (process.argv[2] === 'local') {
-  port = 8080
-}
+// Set the view engine to EJS
+server.set('view engine', 'ejs');
 
-server.listen(port, () => console.log('Ready on localhost!'))
+// Start the server
+const port = process.env.PORT || 8080;
+server.listen(port, () => console.log(`Server running on port ${port}`));
