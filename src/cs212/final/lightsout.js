422cb93db0e4aedefeaa330b950445aa88fc9049
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-12-08 00:58:23 -0700

    Update lightsout.js

--- src/cs212/final/lightsout.js
+++ src/cs212/final/lightsout.js
@@ -1,16 +1,9 @@
-const GRID_SIZE = 6;
+const GRID_SIZE = 5;
 const grid = document.getElementById('grid');
-const startBtn = document.getElementById('startBtn');
-const movesCounter = document.getElementById('movesValue');
 let board = [];
-let movesCount = 0;
-let gameStarted = false;
 
+// Initialize board
 function initializeBoard() {
-  movesCount = 0;
-  movesCounter.textContent = movesCount;
-  grid.innerHTML = '';
-
   for (let i = 0; i < GRID_SIZE; i++) {
     const row = [];
     for (let j = 0; j < GRID_SIZE; j++) {
@@ -18,37 +11,35 @@ function initializeBoard() {
       square.classList.add('square');
       square.addEventListener('click', () => toggleSquare(i, j));
       grid.appendChild(square);
-      row.push(false);
+      row.push(false); // false represents white, true represents black
     }
     board.push(row);
   }
+
+  randomizeBoard();
 }
 
+// Randomize board
 function randomizeBoard() {
-  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
-    const x = Math.floor(i / GRID_SIZE);
-    const y = i % GRID_SIZE;
-    const rand = Math.random() > 0.5;
-    if (rand) {
-      toggleCell(x, y);
-    }
-  }
-}
-
-function toggleSquare(x, y) {
-  if (gameStarted) {
-    movesCounter.textContent = ++movesCount;
-
+  for (let i = 0; i < 15; i++) {
+    const x = Math.floor(Math.random() * GRID_SIZE);
+    const y = Math.floor(Math.random() * GRID_SIZE);
     toggleCell(x, y);
-    toggleCell(x - 1, y);
-    toggleCell(x + 1, y);
-    toggleCell(x, y - 1);
-    toggleCell(x, y + 1);
-
-    checkWin();
   }
 }
 
+// Toggle square and its neighbors
+function toggleSquare(x, y) {
+  toggleCell(x, y);
+  toggleCell(x - 1, y);
+  toggleCell(x + 1, y);
+  toggleCell(x, y - 1);
+  toggleCell(x, y + 1);
+
+  checkWin();
+}
+
+// Toggle individual cell
 function toggleCell(x, y) {
   if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
     const square = grid.children[x * GRID_SIZE + y];
@@ -57,20 +48,13 @@ function toggleCell(x, y) {
   }
 }
 
+// Check for win condition
 function checkWin() {
   const isSolved = board.every(row => row.every(cell => !cell));
   if (isSolved) {
-    window.alert(`You won in ${movesCount} moves!`);
+    window.alert('You win!');
   }
 }
 
-startBtn.addEventListener('click', () => {
-  board = [];
-  initializeBoard();
-  randomizeBoard();
-  gameStarted = true;
-  movesCount = 0;
-  movesCounter.textContent = movesCount;
-});
-
+// Initialize the game
 initializeBoard();
