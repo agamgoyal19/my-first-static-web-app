382da4fdd6eddba31ba4ae4c24d9c2a6905faad4
agamgoyal19 <86164231+agamgoyal19@users.noreply.github.com>
2023-12-08 00:53:56 -0700

    Update lightsout.js

--- src/cs212/final/lightsout.js
+++ src/cs212/final/lightsout.js
@@ -1,20 +1,15 @@
-// Variables
 const GRID_SIZE = 6;
 const grid = document.getElementById('grid');
 const startBtn = document.getElementById('startBtn');
 const movesCounter = document.getElementById('movesValue');
 let board = [];
 let movesCount = 0;
+let gameStarted = false;
 
-// Initialize board
 function initializeBoard() {
   movesCount = 0;
   movesCounter.textContent = movesCount;
-
-  // Clear the board by removing all children
-  while (grid.firstChild) {
-    grid.removeChild(grid.firstChild);
-  }
+  grid.innerHTML = '';
 
   for (let i = 0; i < GRID_SIZE; i++) {
     const row = [];
@@ -23,13 +18,12 @@ function initializeBoard() {
       square.classList.add('square');
       square.addEventListener('click', () => toggleSquare(i, j));
       grid.appendChild(square);
-      row.push(false); // false represents white, true represents black
+      row.push(false);
     }
     board.push(row);
   }
 }
 
-// Randomize board
 function randomizeBoard() {
   for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
     const x = Math.floor(i / GRID_SIZE);
@@ -41,11 +35,9 @@ function randomizeBoard() {
   }
 }
 
-let gameStarted = false;
-// Toggle square and its neighbors
 function toggleSquare(x, y) {
   if (gameStarted) {
-    movesCounter.textContent = movesCount; // Update moves display
+    movesCounter.textContent = ++movesCount;
 
     toggleCell(x, y);
     toggleCell(x - 1, y);
@@ -53,14 +45,10 @@ function toggleSquare(x, y) {
     toggleCell(x, y - 1);
     toggleCell(x, y + 1);
 
-    movesCount++;
-    movesCounter.textContent = movesCount;
-
     checkWin();
   }
 }
 
-// Toggle individual cell
 function toggleCell(x, y) {
   if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
     const square = grid.children[x * GRID_SIZE + y];
@@ -69,7 +57,6 @@ function toggleCell(x, y) {
   }
 }
 
-// Check for win condition
 function checkWin() {
   const isSolved = board.every(row => row.every(cell => !cell));
   if (isSolved) {
@@ -77,18 +64,13 @@ function checkWin() {
   }
 }
 
-// Start button event
 startBtn.addEventListener('click', () => {
   board = [];
-  while (grid.firstChild) {
-    grid.removeChild(grid.firstChild);
-  }
   initializeBoard();
   randomizeBoard();
-  gameStarted = true; // Set gameStarted to true when the game starts
-  movesCount = 0; // Reset moves count to 0 when starting a new game
-  movesCounter.textContent = movesCount; // Update moves display
+  gameStarted = true;
+  movesCount = 0;
+  movesCounter.textContent = movesCount;
 });
 
-// Initialize the game
 initializeBoard();
